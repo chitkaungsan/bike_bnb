6341913137428955caf198cb7e7d674e6b18d615
chitkaungsan <cks.northstar@gmail.com>
2025-11-17 12:36:43 +0700

    ready made for docker

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,17 @@ import vue from '@vitejs/plugin-vue'
 import commonjs from '@originjs/vite-plugin-commonjs';
 
 export default defineConfig({
+     server: {
+        host: true,
+        port: 5173,
+        strictPort: true,
+        hmr: {
+            host: "localhost",
+        },
+        watch: {
+            usePolling: true,
+        },
+    },
     plugins: [
         laravel({
             input: [ 'resources/css/app.scss','resources/js/app.js'],
